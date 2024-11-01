ea88c56e0c4001b8f3c5cb329ce15a47794a90f1
Jeftar <jeftarmascarenhas@hotmail.com>
2024-11-01 10:41:47 -0300

    feat: creating deploy to GFT Coin ICO

---
+++ ignition/modules/GFTCoinICO.ts
@@ -0,0 +1,14 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const GFTCoinICOModule = buildModule("GFTCoinICOModule", (m) => {
+  const beneficiary = m.getAccount(0);
+
+  const gftCoinICO = m.contract("GFTCoinICO", [beneficiary]);
+
+  return { gftCoinICO };
+});
+
+export default GFTCoinICOModule;
